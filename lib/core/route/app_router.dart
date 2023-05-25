65f79e6b528fc8440154d618a29e4589ac09a254
amrkamal1993 <amr2006_kamal@yahoo.com>
2023-05-25 10:31:28 +0300

    Adding app router

---
+++ lib/core/route/app_router.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'package:zajil/core/route/route_names.dart';
+
+class AppRouter {
+  static GoRouter returnRouter() {
+    GoRouter router = GoRouter(
+      initialLocation: loginPage,
+      routes: [
+        GoRoute(
+          name: 'Login',
+          path: loginPage,
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          name: 'index',
+          path: index,
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          name: 'Home',
+          path: homePage,
+          builder: (context, state) => const HomeScreen(),
+        ),
+      ],
+    );
+    return router;
+  }
+}
+
+
+extension AppRouterNavigation on BuildContext {
+
+  void navigateToLogin() {
+    GoRouter.of(this).push(Uri(path: loginPage).toString());
+  }
+
+  void navigateToHome() {
+    GoRouter.of(this).push(Uri(path: homePage).toString());
+  }
+
+}
